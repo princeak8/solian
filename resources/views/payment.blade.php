03b10350e810b76f8b1705230a60f04b6c1c62c5
Michaels042 <mnwachukwu042@gmail.com>
2022-05-31 02:44:23 +0100

    completed payment page

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -5,56 +5,74 @@
     <p>This is the payment page</p>
     <div class="container-fluid breadcrumb-option">
         <div class="row">
-            <div class="col-md-6 col-12 mt-5 payment-left">
-                <div class="link-progress">
-                    <a href="/">Information</a>
-                    <i class='fas fa-greater-than fa-xsm'></i>
-                    <a href="/">Shipping</a>
-                    <i class='fas fa-greater-than fa-xsm'></i>
-                    <a href="/">Payment</a>
-                </div>
-                <h5 style="display:flex; justify-content: center;">Express Checkout</h5>
-                <div class="express-checkout">
-                    <div class="payment-links">
-                        <a href="#"><img src="{{asset('assets/img/payment/payment-1.png')}}" alt=""></a>
-                        <a href="#"><img src="{{asset('assets/img/payment/payment-2.png')}}" alt=""></a>
+            <div class="col-md-7 col-12 mt-5 payment-left">
+                <div class="col-md-8 offset-2 col-12">
+                    <div class="link-progress">
+                        <a href="/">Checkout</a>
+                        <i class="fas fa-greater-than fa-sm"></i>
+                        <h5>Payment</h5>
                     </div>
-                </div>
-                <div class="contact-checkout">
-                    <h5><span>Or</span></h5>
-                </div>
-                <h4>Contact Information</h4>
-                <div class="login-profile">
-                        <a href="#"><img src="{{asset('assets/img/product/product-2.jpg')}}" class="thumbnail" style="border-radius: 50%; height: 3em;"alt=""></a>
-                    <span class="profile-details">
-                        <a href="/"> lilian ekwueme (lilian@email.com)</a>
-                        <a href="/"> Log out</a>
-                    </span>
-                </div>
-                <h5 class="my-3">Shipping Address</h5>
-
-                <div class="contact__form">
-                    <form action="#">
-                        <input type="text" name="savedAddress" placeholder="Saved Address">
-                        <input type="number" name="phoneNumber" placeholder="Phone number">
-                        <span style="display:flex; justify-content: space between;">
-                            <input type="text" name="firstName" placeholder="First Name"class="mr-3 w-75;">
-                            <input type="text" name="lastName" placeholder="Last Name"class="w-75;">
-                        </span>
-                        <textarea  name="address" placeholder="Address"></textarea>
-                        <input type="text" name="city" placeholder="City">
-                        <span style="display:flex; justify-content: space between;">
-                            <input type="text" name="country" placeholder="Country"class="mr-3 w-25;">
-                            <input type="text" name="state" placeholder="State"class="mr-3 w-25;">
-                            <input type="text" name="zipcode" placeholder="Zipcode"class="w-25;">
-                        </span>
-                        <button type="submit" class="site-btn">Continue to shipping</button>
-                    </form>
-                </div>
-               
+                    <div class="card-payment">
+                        <h5 style="display:flex; justify-content: center; margin-bottom: 1em;">Card Payment</h5>
+                        <div class="payment-links">
+                            <a href="#"><img src="{{asset('assets/img/payment/payment-1.png')}}" alt=""></a>
+                            <a href="#"><img src="{{asset('assets/img/payment/payment-2.png')}}" alt=""></a>
+                        </div>
+                    </div>
                 
+                    
+                    <div class="bank-transfer">
+                        <h5><span>Or</span></h5>
+                    </div>
+                    <h4 class="section-title">Bank Transfer</h4><hr>
+                    <div>
+                        <h5>Bank: First Bank</h5>
+                        <h5>Account Name: Lilian Ekueme</h5>
+                        <h5>Account Name: 3001234567</h5>
+                    </div><hr>
+                    <div>
+                        <h5>Bank: Zenith Bank</h5>
+                        <h5>Account Name: Lilian Ekueme</h5>
+                        <h5>Account Name: 2001234567</h5>
+                    </div><hr>
+                    <div class="upload-evidence">
+                        <p>Upload your evidence of payment</p>
+                        <form action="/action_page.php">
+                            <input type="file" id="myFile" class="btn btn-secondary"name="filename">
+                            <input type="submit">
+                        </form>
+                    </div>
+                    <!-- <div class="login-profile">
+                            <a href="#"><img src="{{asset('assets/img/product/product-2.jpg')}}" class="thumbnail" style="border-radius: 50%; height: 3em;"alt=""></a>
+                        <span class="profile-details">
+                            <a href="/"> lilian ekwueme (lilian@email.com)</a>
+                            <a href="/"> Log out</a>
+                        </span>
+                    </div> -->
+                    <!-- <h5 class="my-3">Shipping Address</h5> -->
+
+                    <!-- <div class="contact__form">
+                        <form action="#">
+                            <input type="text" name="savedAddress" placeholder="Saved Address">
+                            <input type="number" name="phoneNumber" placeholder="Phone number">
+                            <span style="display:flex; justify-content: space between;">
+                                <input type="text" name="firstName" placeholder="First Name"class="mr-3 w-75;">
+                                <input type="text" name="lastName" placeholder="Last Name"class="w-75;">
+                            </span>
+                            <textarea  name="address" placeholder="Address"></textarea>
+                            <input type="text" name="city" placeholder="City">
+                            <span style="display:flex; justify-content: space between;">
+                                <input type="text" name="country" placeholder="Country"class="mr-3 w-25;">
+                                <input type="text" name="state" placeholder="State"class="mr-3 w-25;">
+                                <input type="text" name="zipcode" placeholder="Zipcode"class="w-25;">
+                            </span>
+                            <button type="submit" class="site-btn">Continue to Payment</button>
+                        </form>
+                    </div> -->
+                </div>
             </div>
-            <div class="col-md-6 col-12 mt-5" style="height: 100vh; background-color: WhiteSmoke;">
+           
+            <div class="col-md-5 col-12 mt-5" style="height: 100vh; background-color: WhiteSmoke;">
                 <div class="container payment-right">
                     <div class="row pt-5">
                         <div class="col-md-2">
@@ -66,13 +84,12 @@
                         </div>
                         <div class="col-md-3" style="font-weight: bold;">$100</div>
                     </div> <hr>
-                    <div class="contact__form">
+                    <!-- <div class="contact__form">
                         <form action="#">
-                            
                             <input type="text" class="w-50" placeholder="Gift Card/ Discount Voucher">
                             <span class="btn btn-secondary">Apply</span>
                         </form>
-                    </div> <hr>
+                    </div> <hr> -->
                     <div>
                         <div style="display: flex; justify-content: space-between">
                             <span>Subtotal</span>
