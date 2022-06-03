5ca26c47fcdb282b6eb38fdbe9526907d968fc3b
princeak <akalodave@gmail.com>
2022-06-03 10:19:18 +0100

    latest changes

--- resources/views/layouts/public/footer.blade.php
+++ resources/views/layouts/public/footer.blade.php
@@ -374,19 +374,19 @@
                 throw error;
             });
     }
-
-    setInterval(function () {
-        let url = "{{url('update_dropbox_photo_url')}}";
-        axios.get(url)
-        .then((res) => { 
-            //console.log(res.data);
-            console.log('updated dropbox photo urls');
-        })
-        .catch((error) => {
-            console.log("An error occured while trying to set rate "+error.message);
-            throw error;
-        });
-    }, 100000);
+    // n=0
+    // setInterval(function () { n++
+    //     let url = "{{url('update_dropbox_photo_url')}}";
+    //     axios.get(url)
+    //     .then((res) => { 
+    //         //console.log(res.data);
+    //         console.log('updated dropbox photo urls'+n);
+    //     })
+    //     .catch((error) => {
+    //         console.log("An error occured while trying to set rate "+error.message);
+    //         throw error;
+    //     });
+    // }, 100000);
 
     /*
     var cart = new Vue({
